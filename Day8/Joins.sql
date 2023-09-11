568dcb11c82b5212e6fe65ffac716d3d132fbdd2
bhovesh2002 <110907973+bhovesh2002@users.noreply.github.com>
2023-09-11 21:56:41 +0530

    Update Joins.sql

--- Day8/Joins.sql
+++ Day8/Joins.sql
@@ -1,2 +1,12 @@
 SELECT * FROM Employee CROSS JOIN Department;
+
+
 SELECT * FROM Employee WHERE ID>0;
+
+
+SELECT Employee.EmployeeName, Department.DepartmentName
+FROM Employee
+FULL OUTER JOIN Department
+ON Employee.DepartmentID = Department.DepartmentID;
+
+
